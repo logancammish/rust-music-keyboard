11aa2a27bcd7730f370eb0b2deb62d42bc0f5a85
L.J.Cammish <l.j.cammish@student.saintkentigern.com>
2025-06-27 11:23:02 +1200

    fix compiling not including config folder

--- build.rs
+++ build.rs
@@ -1,9 +1,6 @@
 use {
     std::{
-        env,
-        io,
-        fs,
-        path::Path,
+        collections::HashMap, env, fs, io, ops::Deref, path::{Path, PathBuf}
     },
     winresource::WindowsResource,
 };
@@ -19,18 +16,24 @@ fn main() -> io::Result<()> {
     // Copy assets folder to output directory
     let out_dir = env::var("OUT_DIR").unwrap();
     let target_dir = Path::new(&out_dir).ancestors().nth(3).unwrap();
-    let assets_dir = Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("assets");
+    let dirs: HashMap<&str, PathBuf> = vec![ 
+        ("config", Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("config")),
+        ("assets", Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("assets")),
+
+    ].into_iter().collect();
     
-    if assets_dir.exists() {
-        let target_assets = target_dir.join("assets");
-        if target_assets.exists() {
-            fs::remove_dir_all(&target_assets)?;
-        }
-        fs::create_dir_all(&target_assets)?;
-        for entry in fs::read_dir(assets_dir)? {
-            let entry = entry?;
-            let target_path = target_assets.join(entry.file_name());
-            fs::copy(entry.path(), target_path)?;
+    for dir in dirs.iter() { 
+        if dir.1.deref().exists() {
+            let target = target_dir.join(*dir.0);
+            if target.exists() {
+                fs::remove_dir_all(&target)?;
+            }
+            fs::create_dir_all(&target)?;
+            for entry in fs::read_dir(dir.1.deref())? {
+                let entry = entry?;
+                let target_path = target.join(entry.file_name());
+                fs::copy(entry.path(), target_path)?;
+            }
         }
     }
 
